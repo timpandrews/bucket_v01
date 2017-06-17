30c2815ae9cf2c1ace38a30447589ec8c5993f26
Tim Andrews <tim.andrews@imagineifinc.com>
2017-06-17 16:46:17 -0400

    Implement basic bootstrap theme

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from django.contrib import admin
 
-from inventory.views import home
+from inventory.views import home, page1
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^page1/$', page1, name='page1'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
 def home(request):
-    return HttpResponse("<h1>Home</h1>")
+    context = {
+        "title": "Bucket v01",
+    }
+    return render(request, "home.html", context)
+
+def page1(request):
+    context = {
+        "title": "Bucket v01",
+    }
+    return render(request, "page1.html", context)
